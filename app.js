ae7b05c5c156dd6a519fcfe8c7d059e32daad0c4
Kangsik Kevin Lee <kangsik.kevin.lee@gmail.com>
2018-03-26 01:43:30 +0000

    mongo database setup, mongoose package added

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
