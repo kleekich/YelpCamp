aafbd6c60a7718ac1eb2e254321849c61d7113ba
Kangsik Kevin Lee <kangsik.kevin.lee@gmail.com>
2018-03-31 17:52:36 +0000

    Only authorized user can access to edit route for comment

--- routes/comments.js
+++ routes/comments.js
@@ -46,21 +46,43 @@ router.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
 })
 //EDIT Comment route
 router.get("/campgrounds/:id/comments/:comment_id/edit", function(req, res){
-    Comment.findById(req.params.comment_id, function(err, foundComment){
-        if(err){
-            console.log(err);
-            res.redirect("back");
-        }else{
-            res.render("comments/edit",{ campground_id : req.params.id,
-                                         comment: foundComment
-                                        });
-        }
-    })
+    
+    //if user is logged in
+    if(req.isAuthenticated()){
+         //if comment belongs to the user
+         Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                console.log(err);
+                res.redirect("back");
+            }else{
+                console.log("======found comment=====");
+                console.log(foundComment);
+                console.log("========================");
+                
+                if(req.user._id.equals(foundComment.author.id)){
+                    res.render("comments/edit",{ campground_id : req.params.id,
+                                             comment: foundComment
+                                            });
+                }else{
+                    res.redirect("back");
+                }
+            }
+        })
+        
+    }else{
+       res.redirect('/login'); 
+    }
+       
+    //else
+    
+    
+    
+    
 })
 
 //UPDATE Comment route
 router.put("/campgrounds/:id/comments/:comment_id", function(req, res){
-    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+    Comment.findByIdAndUpdate(req.params.comment_id,  req.body.comment, function(err, updatedComment){
         if(err){
             console.log(err);
             res.redirect("back");
@@ -71,7 +93,21 @@ router.put("/campgrounds/:id/comments/:comment_id", function(req, res){
     })
 })
 
+router.delete("/campgrounds/:id/comments/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err, removedComment){
+        if(err){
+            res.redirect("back");
+        }else{
+            
+            res.redirect("/campgrounds/"+req.params.id);
+        }
+    })
+    
+})
+
 //router.put("/campgrounds/:id")
+
+
 
 //Middleware
 function isLoggedIn(req, res, next){
