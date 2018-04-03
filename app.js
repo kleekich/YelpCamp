f174daf251d7fd2c7ca787035d0aac3f4c2a3b97
Kangsik Kevin Lee <kangsik.kevin.lee@gmail.com>
2018-04-03 02:03:24 +0000

    add mLab

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+//mongoose.connect("mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://kleekich:yppi@ds231749.mlab.com:31749/yelpcamp_deployed");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 //Tell express to serve public directory
